8727eb09daac95f695b938fd34b73bab37da11af
Antoine CRONIER <acronier@campus-eni.fr>
2020-09-28 12:53:36 +0200

    Demo1

---
+++ RCDA008F-Xamarin/RCDA008F-Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RCDA008F_Xamarin.Models;
+
+namespace RCDA008F_Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
