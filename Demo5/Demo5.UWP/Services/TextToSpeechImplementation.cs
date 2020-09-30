d55ea22251079e1816aabf82908ffd97d1b4894d
Antoine CRONIER <acronier@campus-eni.fr>
2020-09-30 10:22:42 +0200

    Tp3

---
+++ Demo5/Demo5.UWP/Services/TextToSpeechImplementation.cs
@@ -0,0 +1,26 @@
+﻿using Demo5.Services;
+using Demo5.UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeechImplementation))]
+namespace Demo5.UWP.Services
+    {
+        public class TextToSpeechImplementation : ITextToSpeech
+        {
+            public async void Speak(string text)
+            {
+                var mediaElement = new MediaElement();
+                var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+                var stream = await synth.SynthesizeTextToStreamAsync(text);
+
+                mediaElement.SetSource(stream, stream.ContentType);
+                mediaElement.Play();
+            }
+        }
+    }
